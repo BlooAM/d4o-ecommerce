dbdfcfe136efdd31a9cbae2658ebb751d2fb5860
BlooAM <adam3200@vp.pl>
2025-01-13 08:56:28 +0100

    fix typo

--- d4o_ecommerce/shop/admin.py
+++ d4o_ecommerce/shop/admin.py
@@ -13,4 +13,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
-    prepopulated_fields = {'slig': ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
