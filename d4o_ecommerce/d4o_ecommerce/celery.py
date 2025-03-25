a2652b7e372e2e2b403d2899306a4086b1c9837e
BlooAM <adam3200@vp.pl>
2025-03-25 09:44:18 +0100

    celery config

--- d4o_ecommerce/d4o_ecommerce/celery.py
+++ d4o_ecommerce/d4o_ecommerce/celery.py
@@ -5,3 +5,5 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'd4o_ecommerce.settings')
 
 app = Celery('d4o_ecommerce')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
