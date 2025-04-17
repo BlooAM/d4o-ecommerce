5dc604b427cf5e3b704d5b5e5c951e87fcbd9e30
BlooAM <adam3200@vp.pl>
2025-04-17 15:12:36 +0200

    order created mail notification task

--- d4o_ecommerce/orders/tasks.py
+++ d4o_ecommerce/orders/tasks.py
@@ -2,3 +2,14 @@ from celery import shared_task
 from django.core.mail import send_mail
 
 from .models import Order
+
+
+@shared_task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'Order no. {order_id}'
+    message = f'Hello {order.first_name}, \n\n' \
+              'You have created order in our shop. ' \
+              f'Identifier of the order: {order.id}'
+    mail_sent = send_mail(subject, message, 'admin@d4_ecommerce.com', [order.email])
+    return mail_sent
