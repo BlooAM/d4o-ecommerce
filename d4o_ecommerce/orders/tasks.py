66f7fcaca1379ea5014b3c5df3f328222a5b7d99
BlooAM <adam3200@vp.pl>
2025-04-20 10:37:57 +0200

    mail send fix

--- d4o_ecommerce/orders/tasks.py
+++ d4o_ecommerce/orders/tasks.py
@@ -7,10 +7,11 @@ from .models import Order
 @shared_task
 def order_created(order_id):
     order = Order.objects.get(id=order_id)
+    print(order.email)
     subject = f'Order no. {order_id}'
     message = f'Hello {order.first_name}, \n\n' \
               'You have created order in our shop. ' \
               f'Identifier of the order: {order.id}'
-    print(message)
+
     mail_sent = send_mail(subject, message, 'admin@mail.com', [order.email])
     return mail_sent
