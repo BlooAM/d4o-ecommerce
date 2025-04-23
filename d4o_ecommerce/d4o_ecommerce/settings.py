d9dfb978e82f10ccb6828ffcf0a42f4f316cdfe8
BlooAM <adam3200@vp.pl>
2025-04-23 19:03:44 +0200

    fix typo

--- d4o_ecommerce/d4o_ecommerce/settings.py
+++ d4o_ecommerce/d4o_ecommerce/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
-    'payment.app.PaymentConfig',
+    'payment.apps.PaymentConfig',
 ]
 
 MIDDLEWARE = [
