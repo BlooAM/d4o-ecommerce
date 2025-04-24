70e50e21a6556d19d94721f640663b0212eaefa5
BlooAM <adam3200@vp.pl>
2025-04-24 10:11:22 +0200

    payment views settings

--- d4o_ecommerce/payment/views.py
+++ d4o_ecommerce/payment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from decimal import Decimal
 
-# Create your views here.
+import stripe
+from django.conf import settings
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+
+from orders.models import Order
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_version = settings.STRIPE_API_VERSION
+
+
+def payment_process(request):
+    order_id = request.session.get('order_id', None)
